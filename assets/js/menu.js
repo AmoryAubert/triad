93925a3eb0bd190558d6c7fbe4328e9ecba1db2a
Amory Aubert <amory.aubert@gmail.com>
2019-06-06 16:42:33 +0200

    fin journee

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,7 +3,7 @@ let menu = document.getElementById("menu");
 
 function menu1(){
     menu.innerHTML = `<ul>
-                        <li>TEST</li>
-                        <li>TEST</li>
+                        <li><a href="game.php">Jouer avec les decks</a></li>
+                        <li><a>Jouer en mode random</a></li>
                     </ul>`;
 }
