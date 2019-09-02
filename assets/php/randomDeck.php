3a569f164d05642157eecbffbe2025fd56680023
AmoryAubert <amory.aubert@gmail.com>
2019-09-02 10:16:46 +0200

    fix random func php

--- assets/php/randomDeck.php
+++ assets/php/randomDeck.php
@@ -1,15 +1,28 @@
 <?php
+$file = fopen("assets/txt/random.txt","wb+");
 $dispo = range(1, 18);
 shuffle($dispo);
-$file = fopen("assets/txt/random.txt","rb+");
-for ($i = 0; $i < 10; $i++){
+for ($i = 0; $i < 5; $i++){
     if ($dispo[$i] < 10){
         $dispo[$i] = "f0".$dispo[$i];
     } else {
         $dispo[$i] = "f".$dispo[$i];
     }
     fwrite($file,$dispo[$i]);
-    if ($i < 9){
+    if ($i < 5){
+        fwrite($file,PHP_EOL);
+    }
+}
+$dispo = range(1, 18);
+shuffle($dispo);
+for ($i = 0; $i < 5; $i++){
+    if ($dispo[$i] < 10){
+        $dispo[$i] = "f0".$dispo[$i];
+    } else {
+        $dispo[$i] = "f".$dispo[$i];
+    }
+    fwrite($file,$dispo[$i]);
+    if ($i < 4){
         fwrite($file,PHP_EOL);
     }
 }
